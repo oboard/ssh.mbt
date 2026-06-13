140d283830e844e7bb4a232b86ded4b9b8aa9fac
PaiGack <460852283@qq.com>
2026-06-13 17:12:28 +0800

    chore: 移除平台区分

--- run.sh
+++ run.sh
@@ -12,6 +12,8 @@ export MOONBIT_CLI_ARGS="$MSSH_USERNAME@$MSSH_HOST --port $MSSH_PORT --exec 'una
 # 是否输出 调试信息
 # export MOONBIT_SSH_DEBUG="true"
 
+moon clean
+
 # # 存在 ffi 时，直接 run 会没有任何输出，需要使用 build 后手动运行
 # moon build cmd/main --target native
 
