e0a41775ce1b74825f81190a75f4332063df935d
rzhangsan <4iNtJYlBfhr1QT8bkeMawE+rzhangsan@noreply.cnb.cool>
2026-06-22 13:37:05 +0800

    chore: forward nginx

--- cmd/forward/run.sh
+++ cmd/forward/run.sh
@@ -9,8 +9,20 @@ set +a
 moon clean
 moon build . --target native
 
+# export MOONBIT_SSH_DEBUG="true"
+
+docker rm -f nginx
+
+# 启动一个 nginx bind 到本地的 1080 端口 
+docker run --name nginx -d -p 1080:80 nginx
+
+sleep 5
+
+# 访问 1080 测试 nginx
+curl http://127.0.0.1:1080
+
 # 本地转发示例
-export MOONBIT_CLI_ARGS="$MSSH_USERNAME@$MSSH_HOST --port $MSSH_PORT -L 8080:localhost:80 --password $MSSH_PASSWORD"
+export MOONBIT_CLI_ARGS="$MSSH_USERNAME@$MSSH_HOST --port $MSSH_PORT -L 1080:localhost:80 --password $MSSH_PASSWORD"
 
 # # 远程转发示例
 # export MOONBIT_CLI_ARGS="$MSSH_USERNAME@$MSSH_HOST --port $MSSH_PORT -R 9090:localhost:3000 --password $MSSH_PASSWORD"
